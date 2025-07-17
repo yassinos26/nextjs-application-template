84ae2d2990027abd3498c37bb64db510d7e7385e
yassinos26 <yassine.250mellouli@gmail.com>
2025-07-17 14:56:10 +0100

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752760570111

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b78e2ce0._.js",
+  "static/chunks/node_modules_d327b860._.js"
+],
+    source: "dynamic"
+});
